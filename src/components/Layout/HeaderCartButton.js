05086216e9635eb2e0f286be69642491cb8c1b52
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-07-28 18:00:51 +0530

    Cart (open && open)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,9 @@
-import React from "react";
-import classes from "./HeaderCartButton.module.css";
-import CartIcon from "../Cart/CartIcon";
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
 
-const HaederCartButton = (porps) => {
+const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
@@ -14,4 +13,4 @@ const HaederCartButton = (porps) => {
   );
 };
 
-export default HaederCartButton;
+export default HeaderCartButton;
